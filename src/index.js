389e8d7718a5ec8dd2d3cdaae2ae7cc49a8cd791
Fotyniya <s.kozachenko@ekwo.dp.ua>
2023-01-18 19:59:03 +0200

    work

--- src/index.js
+++ src/index.js
@@ -6,18 +6,24 @@ import { createMarkup } from './partials/markup.js';
 
 const API_KEY = '32817596-3735423159e4b61dcdcaf4a45';
 const BASE_URL = 'https://pixabay.com/api/';
+let page = 1;
+const per_page = 40;
 
 const fieldSearch = document.querySelector('[name = searchQuery]');
 const submitBtn = document.querySelector('button');
 const imgContainer = document.querySelector('.gallery');
 const btnNextRequest = document.querySelector('.load-more');
 
-fieldSearch.addEventListener('input', event => console.log(`${fieldSearch.value}`));
-    let page = 1;
-    const per_page = 40;
+fieldSearch.addEventListener('input', event => {
+    if (!fieldSearch.value){
+        onClearMarkup();
+      
+    }
+});
 
 submitBtn.addEventListener('click', evt => {
     evt.preventDefault();
+    
     onRequest();
 });
 
@@ -41,7 +47,7 @@ async function onRequest() {
                 gallery = new SimpleLightbox('.gallery a');
                 gallery.refresh();
                 smoothScroll ();
-                
+                console.log(page)
             } else {
                 Notify.warning("We're sorry, but you've reached the end of search results.")
                 btnNextRequest.style.visibility = 'hidden';
@@ -57,7 +63,8 @@ async function onRequest() {
 
 function onClearMarkup() {
     imgContainer.innerHTML = "";  
-    btnNextRequest.style.visibility = 'hidden';  
+    btnNextRequest.style.visibility = 'hidden';
+      
 }
 function smoothScroll () {
     const { height: cardHeight } = imgContainer.firstElementChild.getBoundingClientRect();
